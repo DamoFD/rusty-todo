f653f043596fa38e45de5d66071edd47cfa90464
DamoFD <voshall@yahoo.com>
2023-09-23 20:28:08 -0700

    created db tests
    created db tests

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,7 +1,64 @@
+use super::{Todo, TodoMac};
+use super::db::Db;
+use crate::model;
+use crate::model::db::init_db;
+use crate::model::todo::{TodoPatch, TodoStatus};
 
 #[derive(sqlx::FromRow, Debug, Clone)]
 pub struct Todo {
     pub id: i64,
     pub cid: i64, //creator id
     pub title: String,
+    pub status: TodoStatus,
 }
+
+#[derive(sqlb::Fields, Default, Clone)]
+pub struct TodoPatch {
+    pub cid: Option<i64>,
+    pub title: Option<String>,
+    pub status: Option<TodoStatus>,
+}
+
+#[derive(sqlx::Type, Debug, Clone, PartialEq, Eq)]
+#[sqlx(type_name = "todo_status_enum")]
+#[sqlx(rename_all = "lowercase")]
+pub enum TodoStatus {
+    Open,
+    Close,
+}
+sqlb::bindable!(TodoStatus);
+
+pub struct TodoMac;
+
+impl TodoMac {
+    pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
+        // let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status";
+        // let query = sqlx::query_as::<_, Todo>(&sql)
+        //    .bind(123 as i64) // FIXME - should come from user context
+        //    .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+
+        let sb = sqlb::insert()
+            .table("todo")
+            .data(data.fields())
+            .returning(&["id", "cid", "title", "status"]);
+
+        let todo = sb.fetch_one(db).await?;
+
+        Ok(todo)
+    }
+
+    pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
+        let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+
+        // build the sqlx-query
+        let query = sqlx::query_as(&sql);
+        // execute the query
+        let todos = query.fetch_all(db).await?;
+
+        Ok(todos)
+    }
+}
+
+#[cfg(test)]
+#[path = "../_tests/model_todo.rs"]
+mod tests;
--- backend/target/debug/deps/rusty_todo-dd7d274ba78cd91c.d
+++ backend/target/debug/deps/rusty_todo-dd7d274ba78cd91c.d
@@ -1,11 +1,12 @@
-/home/damofd/Projects/rusty-todo/backend/target/debug/deps/rusty_todo-dd7d274ba78cd91c: src/main.rs src/model/mod.rs src/model/db.rs src/model/../_tests/model_db.rs src/model/todo.rs src/security/mod.rs src/web/mod.rs
+/home/damofd/Projects/rusty-todo/backend/target/debug/deps/rusty_todo-dd7d274ba78cd91c: src/main.rs src/model/mod.rs src/model/db.rs src/model/../_tests/model_db.rs src/model/todo.rs src/model/../_tests/model_todo.rs src/security/mod.rs src/web/mod.rs
 
-/home/damofd/Projects/rusty-todo/backend/target/debug/deps/rusty_todo-dd7d274ba78cd91c.d: src/main.rs src/model/mod.rs src/model/db.rs src/model/../_tests/model_db.rs src/model/todo.rs src/security/mod.rs src/web/mod.rs
+/home/damofd/Projects/rusty-todo/backend/target/debug/deps/rusty_todo-dd7d274ba78cd91c.d: src/main.rs src/model/mod.rs src/model/db.rs src/model/../_tests/model_db.rs src/model/todo.rs src/model/../_tests/model_todo.rs src/security/mod.rs src/web/mod.rs
 
 src/main.rs:
 src/model/mod.rs:
 src/model/db.rs:
 src/model/../_tests/model_db.rs:
 src/model/todo.rs:
+src/model/../_tests/model_todo.rs:
 src/security/mod.rs:
 src/web/mod.rs:
